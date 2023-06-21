9ee049c2b9bbbd0fd77e1d2ac0352426949ec715
김한울 <gimhan-ul@gimhan-ul-ui-MacBookAir.local>
2023-06-21 15:03:37 +0900

    add-chose-date

--- code.js
+++ code.js
@@ -1,18 +1,37 @@
-//code.js
+// code.js
+
 const TodoInput = document.querySelector(".todo-input");
-const min = document.querySelector(".min");
-const hour = document.querySelector(".hour");
+const deadline = document.querySelector(".deadline");
 const submit = document.querySelector(".submit");
 const body = document.querySelector("body");
 const todos = document.querySelector(".todos");
-const btn = document.querySelectorAll(".btn");
+
+function updateRemainingTime(countdown, selectedDate) {
+    const currentTime = new Date();
+    const timeDifference = selectedDate.getTime() - currentTime.getTime();
+
+    if (timeDifference > 0) {
+        let days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((timeDifference / (1000 * 60 * 60)) % 24);
+        let minutes = Math.ceil((timeDifference / (1000 * 60)) % 60);
+
+        if (minutes === 60) {
+            minutes = 0;
+            hours++;
+        }
+
+        countdown.innerHTML = `남은 시간: ${days}일 ${hours}시간 ${minutes}분`;
+    } else {
+        countdown.innerHTML = '마감되었습니다.'; // 시간이 지났을 때 표시할 내용
+    }
+}
 
 function createTodo() {
     var btn = document.createElement("input");
     var todo = document.createElement("div");
-    var TodoForm= document.createElement("div");
+    var TodoForm = document.createElement("div");
+    var countdown = document.createElement("div"); // 추가: 남은 시간을 표시할 요소
     todo.innerText = TodoInput.value;
-    btn.innerText = "X";
     btn.type = "checkbox";
     todo.classList.add("todo");
     btn.classList.add("btn");
@@ -22,41 +41,77 @@ function createTodo() {
         todos.appendChild(TodoForm);
         TodoForm.appendChild(btn);
         TodoForm.appendChild(todo);
+        TodoForm.appendChild(countdown); // 추가: 남은 시간을 표시할 요소를 할 일 요소에 추가
     }
-    if (!(hour.value == "" && min.value == "")) {
-        setTimeout(function() {
-            var bell = new Audio('./bell.mp3');
-            bell.play();
-            var dialog = document.createElement("dialog");
-            var form = document.createElement("form");
-            var closeBtn = document.createElement("button");
-            dialog.classList.add("dialog");
-            form.classList.add("form");
-            closeBtn.classList.add("closeBtn");
-            dialog.innerText = TodoInput.value;
-            closeBtn.innerText = "close";
-            body.appendChild(dialog);
-            dialog.appendChild(form);
-            form.appendChild(closeBtn);
-            dialog.showModal();
-            btn.click();
-          }, Number(hour.value) * 3600000 + Number(min.value) * 60000);      
+    if (deadline.value !== "") {
+        const selectedDate = new Date(deadline.value);
+        const currentTime = new Date();
+        const timeDifference = selectedDate.getTime() - currentTime.getTime();
+
+        if (timeDifference > 0) {
+            let days = Math.floor(timeDifference / (1000 * 60 * 60 * 24)); // 남은 일수 계산
+            let hours = Math.floor((timeDifference / (1000 * 60 * 60)) % 24); // 남은 시간 계산
+            let minutes = Math.ceil((timeDifference / (1000 * 60)) % 60); // 남은 분 계산
+
+            // 추가: 분이 60분일 경우 시간으로 반올림
+            if (minutes === 60) {
+                minutes = 0;
+                hours++;
+            }
+
+            // 추가: 남은 시간을 표시
+            countdown.innerHTML = `남은 시간: ${days}일 ${hours}시간 ${minutes}분`;
+
+            // 추가: 남은 시간을 업데이트하기 위해 setInterval 사용
+            const updateInterval = setInterval(function() {
+                updateRemainingTime(countdown, selectedDate);
+            }, 60000); // 1분(60초)마다 업데이트
+
+            // 추가: 할 일이 삭제되면 업데이트 Interval도 제거
+            btn.addEventListener("click", function() {
+                clearInterval(updateInterval);
+                removeTodo.call(this);
+            });
+
+            setTimeout(function() {
+                var msg = todos.firstChild.children[1].innerText;
+                var bell = new Audio('./bell.mp3');
+                bell.play();
+                var dialog = document.createElement("dialog");
+                var form = document.createElement("form");
+                var closeBtn = document.createElement("button");
+                dialog.classList.add("dialog");
+                form.classList.add("form");
+                closeBtn.classList.add("closeBtn");
+                dialog.innerText = msg;
+                closeBtn.innerText = "X";
+                body.appendChild(dialog);
+                dialog.appendChild(form);
+                form.appendChild(closeBtn);
+                dialog.showModal();
+                btn.click();
+            }, timeDifference);
+
+            // 추가: 1분마다 남은 시간을 업데이트
+            setInterval(function() {
+                updateRemainingTime(countdown, selectedDate);
+            }, 60000);
+        }
     }
 }
 
-function removeTodo(event) {
+function removeTodo() {
+    const elem = this.parentElement;
     setTimeout(function() {
-        const elem = event.target.parentElement;
         elem.remove();
-      }, 300);
+    }, 300);
 }
-TodoInput.addEventListener("keydown", function(event) {
-    // Check if the pressed key is Enter (keyCode 13)
-    if (event.keyCode === 13) {
-      event.preventDefault(); // Prevent the default Enter key behavior (form submission)
 
-      // Call the createTodo function
-      createTodo();
+TodoInput.addEventListener("keydown", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        createTodo();
     }
-  });
+});
+
 submit.addEventListener("click", createTodo);
